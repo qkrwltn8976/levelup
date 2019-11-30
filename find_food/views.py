807cb8e6ad23363ffd8219efad7b92cdfa851619
HyeongChanHam <56499630+HyeongChanHam@users.noreply.github.com>
2019-12-01 05:30:29 +0900

    add form

--- find_food/views.py
+++ find_food/views.py
@@ -3,11 +3,22 @@ from .models import Vendor, Location
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import LocationSerializer
+# from .forms import PostForm
+
+
 # Create your views here.
 def home(request):
-    vendor = Vendor.objects
-    location = Location.objects.all()
-    return render(request, 'home.html',{'location':location})
+    if request.method == 'POST':
+        
+        loc = request.POST.getlist("location")
+        vendor = Vendor.objects.filter(loc__name__in = loc)
+        location = Location.objects.all()
+        print(vendor)
+        return render(request, 'map.html', {'location':location, 'vendor':vendor})
+        pass
+    else:
+        location = Location.objects.all()
+        return render(request, 'home.html',{'location':location})
 
 def map(request):
     return render(request, 'map.html')
