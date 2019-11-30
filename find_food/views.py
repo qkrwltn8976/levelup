9d03ac63de7aad72203133c127d2bc505bb36d03
Ubuntu <ubuntu@ip-172-31-33-183.ap-northeast-2.compute.internal>
2019-11-30 17:44:07 +0000

    merge

--- find_food/views.py
+++ find_food/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 from .models import Vendor, Location
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import LocationSerializer
 # Create your views here.
 def home(request):
     vendor = Vendor.objects
-    location = Location.objects
-    return render(request, 'home.html',{'vendor':vendor, 'location':location})
+    location = Location.objects.all()
+    return render(request, 'home.html',{'location':location})
 
 def map(request):
     return render(request, 'map.html')
+
+@api_view(['POST'])
+def select_loc(request, loc_id):
+    if request.method == 'POST':
+        location = Location.objects.get(pk=loc_id)
+        serializer = LocationSerializer(location)
+        return Response(serializer.data)
