9c339e9daf3e771399246e0f094cfacac20e0b67
Ubuntu <ubuntu@ip-172-31-33-183.ap-northeast-2.compute.internal>
2019-11-30 19:40:56 +0000

    update

--- find_food/views.py
+++ find_food/views.py
@@ -3,6 +3,7 @@ from .models import Vendor, Location
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import LocationSerializer
+from .serializers import VendorSerializer
 # Create your views here.
 def home(request):
     vendor = Vendor.objects
@@ -10,11 +11,20 @@ def home(request):
     return render(request, 'home.html',{'location':location})
 
 def map(request):
+
     return render(request, 'map.html')
 
 @api_view(['POST'])
 def select_loc(request, loc_id):
     if request.method == 'POST':
         location = Location.objects.get(pk=loc_id)
+        vendor = Vendor.objects.filter(loc=loc_id)
+        print(vendor[0].position_x)
         serializer = LocationSerializer(location)
-        return Response(serializer.data)
+        serializer_v = VendorSerializer(vendor, many=True) 
+        Serializer_list = [serializer.data, serializer_v.data]
+
+        content = {
+            'data': Serializer_list,
+        }
+        return Response(content)
